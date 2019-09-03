a5cd96adafa89b0a92ca27e9a8c26c7c34133f40
Mike Harley <mharley12345@gmail.com>
2019-09-03 14:33:39 -0700

    MVP

--- git-user-cards/src/App.js
+++ git-user-cards/src/App.js
@@ -1,13 +1,37 @@
-import React from 'react';
+import React from "react";
+import "./App.css";
+import axios from "axios";
+import UserCard from "./Components/UserCard"
 
-import './App.css';
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
 
-function App() {
-  return (
-    <div className="App">
-     <h1>Git Users Cards Class Component Style</h1>
-    </div>
-  );
+  componentDidMount() {
+    this.getUser();
+  }
+
+  getUser = () => {
+    axios
+      .get(`https://api.github.com/users/mharley12345`)
+      .then(response => {
+        this.setState({ users: response.data });
+      })
+      .catch(err => console.log(err));
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <UserCard user={this.state.users} />
+  
+      </div>
+    );
+  }
 }
 
-export default App;
+export default App
---
+++ git-user-cards/src/Components/UserCard.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Followers from "./Followers"
+
+const UserCard = props => {
+  return (
+    <div>
+      <img src={props.user.avatar_url} alt={`This is ${props.user.name}`} />
+      <h1>Name: {props.user.name}</h1>
+      <p>User Name : {props.user.login}</p>
+      <h4>Location : {props.user.location}</h4>
+      <h3>Followers:</h3>
+      <Followers/>
+   
+    
+    </div>
+  );
+};
+export default UserCard
